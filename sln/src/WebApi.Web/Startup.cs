cbd2ba9bda237d9cffdd36ae49566c6023db1709
Sebastian Andres <sebandres@gmail.com>
2018-06-19 11:14:12 +1000

    Added Api versioning

--- sln/src/WebApi.Web/Startup.cs
+++ sln/src/WebApi.Web/Startup.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
     using Autofac;
@@ -10,6 +11,7 @@
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.HttpsPolicy;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ApiExplorer;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
@@ -29,7 +31,20 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+            // note: the specified format code will format the version as "'v'major[.minor][-status]"
+            services.AddMvcCore().AddVersionedApiExplorer(
+                options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
+                } );
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddApiVersioning(o => o.ReportApiVersions = true);
 
             services.AddHttpsRedirection(options =>
             {
@@ -37,10 +52,26 @@
             });  
 
             // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info { Title = "WebApi", Version = "v1" });
-            });
+            services.AddSwaggerGen(
+                options =>
+                {
+                    // resolve the IApiVersionDescriptionProvider service
+                    // note: that we have to build a temporary service provider here because one has not been created yet
+                    var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                    // add a swagger document for each discovered API version
+                    // note: you might choose to skip or document deprecated API versions differently
+                    foreach ( var description in provider.ApiVersionDescriptions )
+                    {
+                        options.SwaggerDoc( description.GroupName, CreateInfoForApiVersion( description ) );
+                    }
+
+                    // add a custom operation filter which sets default values
+                    // options.OperationFilter<SwaggerDefaultValues>();
+
+                    // integrate xml comments
+                    // options.IncludeXmlComments( XmlCommentsFilePath );
+                } );
         }
 
         // ConfigureContainer is where you can register things directly
@@ -55,7 +86,7 @@
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -73,12 +104,46 @@
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
             // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi V1");
-            });
+            app.UseSwaggerUI(
+                options =>
+                {
+                    // build a swagger endpoint for each discovered API version
+                    foreach ( var description in provider.ApiVersionDescriptions )
+                    {
+                        options.SwaggerEndpoint( $"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant() );
+                    }
+                } );
     
             app.UseMvc();
+        }
+/*
+        static string XmlCommentsFilePath
+        {
+            get
+            {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var fileName = typeof( Startup ).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine( basePath, fileName );
+            }
+        }
+*/
+        static Info CreateInfoForApiVersion( ApiVersionDescription description )
+        {
+            var info = new Info()
+            {
+                Title = $"WebApi {description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "WebApi description for this project.",
+                Contact = new Contact() { Name = "EDH Support", Email = "support@everydayhero.com" },
+                License = new License() { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+            };
+
+            if ( description.IsDeprecated )
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
         }
     }
 }
